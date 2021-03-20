996a96ed5dc74c26176638807761989e138495d1
Your mato <you@example.com>
2021-03-20 20:42:47 +0800

    Initial Commit

---
+++ OneAtATime/OneAtATimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneAtATimeApp.swift
+//  OneAtATime
+//
+//  Created by kittawat phuangsombat on 2021/3/20.
+//
+
+import SwiftUI
+
+@main
+struct OneAtATimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
